98d086de55ceab3e505d5fe0cca6baee6fbf3aba
DiegoCorrea <diegocorrea.cc@gmail.com>
2016-05-06 02:36:28 -0300

    add struct, quicksort and prints

--- main.c
+++ main.c
@@ -1,77 +1,92 @@
 # include <stdio.h>
+# include <stdlib.h>
 # define MAX 10000
 
+typedef struct item{
+  int chave;
+  int valor;
+  float peso;
+  float lucro;
+  int escolhido;
+  float porcao;
+}itemType;
+
+itemType item[MAX], *emOrdem[MAX];
+
 int capacidade, n, valores[MAX], X[MAX], lucroSomando;
-float pesos[MAX];
+float pesos[MAX], lucro[MAX], lucroMaximoFracionario;
 int escolhidos[MAX];
 
 void input();
-int knapsack3(int );
-void output();
+void outputInput();
+void outputFraction();
+
+/* Ordenar */
+int partition(int inicio, int final);
+int randomizedPartition(int inicio, int final);
+void quickSort(int inicio, int final);
 
 int main(){
   
   input();
 
-  knapsack(0);
-
-  output();
+  quickSort(0, n-1);
+  outputInput();
+  
+  //outputFraction();
 
   return 0;
 }
 
+/*  Tratamento de entrada e saida */
 void input(){
   int i;
 
   scanf("%d", &n);
 
-  for(i = 0; i < n; scanf("%d",&valores[i]), i++);
+  for(i = 0; i < n; scanf("%d",&item[i].valor), i++);
 
-  for(i = 0; i < n; scanf("%f",&pesos[i]), i++);
+  for(i = 0; i < n; scanf("%f",&item[i].peso), i++);
 
   scanf("%d", &capacidade);
+
+  for(i = 0; i < n; i++){
+    item[i].lucro = item[i].valor/item[i].peso;
+    item[i].chave = i+1;
+    emOrdem[i] = &item[i];
+  }
 }
 
-void output(){
+void outputInput(){
   int i;
 
   printf("Entrada\n");
 
   printf("Quantidade de itens: %d\n", n);
-
-  printf("Valores: ");
-  for (i = 0; i < n; i++) printf("%d ", valores[i]);
-  printf("\n");
-
-  printf("Pesos: ");
-  for (i = 0; i < n; i++) printf("%.2f ", pesos[i]);
-  printf("\n");
-
   printf("Capacidade da mochila: %d\n", capacidade);
+  for (i = 0; i < n; i++){ 
+    printf("i%d -> V: %d peso:%.1f lucro:%.1f\n", item[i].chave, item[i].valor, item[i].peso, item[i].lucro);
+  }
+  printf("\n");
+  for (i = 0; i < n; i++){ 
+    printf("i%d\n", emOrdem[i]->chave);
+  }
+  printf("\n");
 }
 
-void knapsack3(int l){
+void outputFraction(){
+  int i;
 
-  if((l = n) && (lucroSomando > OptP)){
-    OptP = lucroSomando;
-    //OptX = ;
-  }
+  printf("Resultado francionario\n");
 
-  if (l = n){
-    C[l] = 0;
-  } else {
-    if (CurW + pesos[l] < capacidade) {
-      C[l] = 1;      
-    } else {
-      C[l] = 0;
-    }
-  }
-  B = lucroSomando + RKnap();
-  for(;;){
-    if() return;
+  printf("Valor: %.1f\n", lucroMaximoFracionario);
+
+  for(i = 0; i < n ; i++){
+    if(item[i].porcao > 0)
+      printf("i%d %1.f\n", item[i].chave, item[i].porcao);
   }
 }
-
+/*  Mochila Fracionaria*/
 int RKnap(){
   int i = 0, j = 0, profit;
   float weight;
@@ -93,4 +108,67 @@ int RKnap(){
     }
   }
   return profit;
+}
+
+/*
+void knapsack3(int l){
+
+  if((l = n) && (lucroSomando > OptP)){
+    OptP = lucroSomando;
+    //OptX = ;
+  }
+
+  if (l = n){
+    C[l] = 0;
+  } else {
+    if (CurW + pesos[l] < capacidade) {
+      C[l] = 1;      
+    } else {
+      C[l] = 0;
+    }
+  }
+  B = lucroSomando + RKnap();
+  for(;;){
+    if() return;
+  }
+}
+*/
+
+/*  Quick Sort para ordenar os lucros fracionarios*/
+int partition(int inicio, int final){
+  int i, j;
+  float pivo;
+  itemType *tmp;
+
+  i = inicio - 1;
+  pivo = emOrdem[final]->lucro;
+  for (j = inicio; j < final - 1; j++){
+    if (emOrdem[j]->lucro <= pivo){
+      i++;
+      tmp = emOrdem[i];
+      emOrdem[i] = emOrdem[j];
+      emOrdem[j] = tmp;
+    }
+  }
+  tmp = emOrdem[i + 1];
+  emOrdem[i + 1] = emOrdem[final];
+  emOrdem[final] = tmp;
+  return i + 1;
+}
+int randomizedPartition(int inicio, int final){
+  int i = (rand() % final + inicio);
+  itemType *tmp;
+  
+  tmp = emOrdem[i];
+  emOrdem[i] = emOrdem[inicio];
+  emOrdem[inicio] = tmp;
+  return partition(inicio,final);
+}
+void quickSort(int inicio, int final){
+  int meio;
+  if (inicio < final){
+    meio = randomizedPartition(inicio,final);
+    quickSort(inicio,meio - 1);
+    quickSort(meio + 1,final);
+  }
 }
